c61a02a59f920988e82f3e8c6a67fdebc5492823
Tanuja <tanujatiwari04@gmail.com>
2021-05-25 23:50:33 +0530

    positioned the navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,39 +2,40 @@ let canvas = document.querySelector('canvas');
 let c = canvas.getContext('2d');
 let isDrawing = false;
 
-function dimensions(){
+function dimensions() {
     canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;    
+    canvas.width = window.innerWidth;
 }
 
-function startDrawing(e){
+function startDrawing(e) {
     isDrawing = true;
     draw(e);
 }
 
-function finishDrawing(){
+function finishDrawing() {
     isDrawing = false;
     c.beginPath();
 }
 
-function draw(e){
-    if(!isDrawing)
+function draw(e) {
+    if (!isDrawing)
         return;
-    c.lineWidth=5;
-    c.lineCap="round";
-    c.lineTo(e.clientX,e.clientY);
+    c.lineWidth = 5;
+    c.lineCap = "round";
+    c.lineTo(e.clientX, e.clientY);
     c.stroke();
     c.beginPath();
-    c.moveTo(e.clientX,e.clientY);
+    c.moveTo(e.clientX, e.clientY);
 }
 
-window.addEventListener('resize',function(){
+window.addEventListener('resize', function () {
     dimensions();
 });
 
-window.addEventListener('load',function(){
+window.addEventListener('load', function () {
     dimensions();
-    canvas.addEventListener('mousedown',startDrawing);
-    canvas.addEventListener('mouseup',finishDrawing);
-    canvas.addEventListener('mousemove',draw);
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mouseup', finishDrawing);
+    canvas.addEventListener('mousemove', draw);
 });
+
